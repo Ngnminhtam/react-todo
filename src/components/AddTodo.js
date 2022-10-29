d8442870714b1c5f60968bfd4be5fc9d83327aae
Ngnminhtam <vhminhtam1@gmail.com>
2022-10-30 00:47:00 +0700

    first commit

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -20,7 +20,7 @@ export default function AddTodo() {
       <div className="input_container">
         <input
           type="text"
-          placeholder="Write todo..."
+          placeholder="Write a todo..."
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
